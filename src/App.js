42b01ec81b9b7c3d7645fc6a6ce11af006fc30a0
Shane Jeffery <nobull.wow@gmail.com>
2020-02-13 23:44:44 -0800

    Continued progress on intake form and starting validation for the form.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import LandingPage from './pages/LandingPage';
+// import NewAccountPage from './pages/NewAccountPage';
+// import DisqualificationPage from './pages/DiqualificationPage';
 
 function App() {
-  return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/">
-              <LandingPage />
-            </Route>
-            {/*<Route path="/about">*/}
-            {/*  <About />*/}
-            {/*</Route>*/}
-            {/*<Route path="/dashboard">*/}
-            {/*  <Dashboard />*/}
-            {/*</Route>*/}
-          </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/">
+                        <LandingPage/>
+                    </Route>
+                    {/*<Route path="/new-account">*/}
+                    {/*  <NewAccountPage />*/}
+                    {/*</Route>*/}
+                    {/*<Route path="/disqualified">*/}
+                    {/*  <DisqualificationPage />*/}
+                    {/*</Route>*/}
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Button,
     TextField,
@@ -6,11 +6,13 @@ import {
     Grid,
     Typography,
     Container,
-    Avatar
+    Avatar,
+    InputAdornment
 } from '@material-ui/core';
 import DirectionsCarIcon from '@material-ui/icons/DirectionsCar';
 import {makeStyles} from '@material-ui/core/styles';
-import MaterialUIForm from 'react-material-ui-form'
+import Helmet from "react-helmet";
+import CurrencyTextField from '@unicef/material-ui-currency-textfield';
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -33,10 +35,80 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function LandingPage() {
+    const [autoPurchasePriceValue, setAutoPurchasePriceValue] = useState();
+    const [autoMakeValue, setAutoMakeValue] = useState();
+    const [autoModelValue, setAutoModelValue] = useState();
+    const [userEstimatedYearlyIncomeValue, setUserEstimatedYearlyIncomeValue] = useState();
+    const [userEstimatedCreditScoreValue, setUserEstimatedCreditScoreValue] = useState();
+
+    const [autoPurchasePriceErrorText, setAutoPurchasePriceErrorText] = useState('');
+    const [autoMakeErrorText, setAutoMakeErrorText] = useState('');
+    const [autoModelErrorText, setAutoModelErrorText] = useState('');
+    const [userEstimatedYearlyIncomeErrorText, setUserEstimatedYearlyIncomeErrorText] = useState('');
+    const [userEstimatedCreditScoreErrorText, setUserEstimatedCreditScoreErrorText] = useState('');
+
     const classes = useStyles();
+
+    function handleFormSubmit(event) {
+
+        event.preventDefault();
+
+        let errorFound = false;
+
+        setAutoPurchasePriceErrorText('');
+        setAutoMakeErrorText('');
+        setAutoModelErrorText('');
+        setUserEstimatedYearlyIncomeErrorText('');
+        setUserEstimatedCreditScoreErrorText('');
+
+        console.log(autoPurchasePriceValue);
+
+        if (!autoPurchasePriceValue) {
+            errorFound = true;
+            setAutoPurchasePriceErrorText('Field must have a value.');
+        }
+
+        if (!autoMakeValue) {
+            errorFound = true;
+            setAutoMakeErrorText('Field must have a value.');
+        }
+
+        if (!autoModelValue) {
+            errorFound = true;
+            setAutoModelValue('Field must have a value.');
+        }
+
+        if (!userEstimatedYearlyIncomeValue) {
+            errorFound = true;
+            setUserEstimatedYearlyIncomeValue('Field must have a value.');
+        }
+
+        if (!userEstimatedCreditScoreValue) {
+            errorFound = true;
+            setUserEstimatedCreditScoreValue('Field must have a value.');
+        } else if (parseInt(userEstimatedCreditScoreValue) <= 300 || parseInt(userEstimatedCreditScoreValue) >= 850) {
+            errorFound = true;
+            setUserEstimatedCreditScoreValue('Credit score must be between 300 and 850.');
+        }
+
+        // If no errors were found, continue the form submission.
+        if (!errorFound) {
+
+
+
+        }
+
+
+
+
+    }
 
     return (
         <Container component="main" maxWidth="xs">
+            <Helmet>
+                <title>Qualification Form for Auto Loan</title>
+            </Helmet>
+
             <CssBaseline/>
             <div className={classes.paper}>
                 <Avatar className={classes.avatar}>
@@ -45,64 +117,100 @@ export default function LandingPage() {
                 <Typography component="h1" variant="h5">
                     Qualification Form for Auto Loan
                 </Typography>
-                <MaterialUIForm className={classes.form}>
+                <form className={classes.form} onSubmit={(event) => handleFormSubmit(event)}>
 
-                    <TextField
+                    <CurrencyTextField
                         variant="outlined"
                         margin="normal"
-                        required
                         fullWidth
                         id="auto-purchase-price"
                         label="Auto Purchase Price"
                         name="auto-purchase-price"
+                        textAlign="left"
+                        value={autoPurchasePriceValue}
+                        onChange={(event, value) => setAutoPurchasePriceValue(value)}
+                        helperText={autoPurchasePriceErrorText}
+                        error={!!autoPurchasePriceErrorText}
                     />
 
                     <TextField
                         variant="outlined"
                         margin="normal"
-                        required
                         fullWidth
                         id="auto-make"
                         label="Auto Make"
                         name="auto-make"
+                        value={autoMakeValue}
+                        onChange={(event) => setAutoMakeValue(event.target.value)}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start"/>
+                            )
+                        }}
+                        helperText={autoMakeErrorText}
+                        error={!!autoMakeErrorText}
                     />
 
                     <TextField
                         variant="outlined"
                         margin="normal"
-                        required
                         fullWidth
                         id="auto-model"
                         label="Auto Model"
                         name="auto-model"
+                        value={autoModelValue}
+                        onChange={(event) => setAutoModelValue(event.target.value)}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start"/>
+                            )
+                        }}
+                        helperText={autoModelErrorText}
+                        error={!!autoModelErrorText}
                     />
 
-                    <TextField
+                    <CurrencyTextField
                         variant="outlined"
                         margin="normal"
-                        required
                         fullWidth
                         id="user-estimated-yearly-income"
                         label="User Estimated Yearly Income"
                         name="user-estimated-yearly-income"
+                        textAlign="left"
+                        value={userEstimatedYearlyIncomeValue}
+                        onChange={(event, value) => setUserEstimatedYearlyIncomeValue(value)}
+                        helperText={userEstimatedYearlyIncomeErrorText}
+                        error={!!userEstimatedYearlyIncomeErrorText}
                     />
 
                     <TextField
                         variant="outlined"
                         margin="normal"
-                        required
                         fullWidth
                         name="user-estimated-credit-score"
                         label="User Estimated Credit Score"
                         id="user-estimated-credit-score"
+                        value={userEstimatedCreditScoreValue}
+                        onChange={(event) => setUserEstimatedCreditScoreValue(event.target.value)}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start"/>
+                            )
+                        }}
+                        helperText={userEstimatedCreditScoreErrorText}
+                        error={!!userEstimatedCreditScoreErrorText}
                     />
 
                     <Grid container>
                         <Grid item xs>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat ante id elit cursus, a interdum nunc venenatis.
-                            Cras nulla nibh, euismod non turpis a, ullamcorper feugiat leo. Pellentesque elementum sapien tellus, quis blandit
-                            tortor malesuada eget. Integer vel neque in dolor euismod fermentum et a nulla. Pellentesque ultricies est
-                            dignissim malesuada aliquet. Nullam non ornare nibh. Orci varius natoque penatibus et magnis dis parturient montes,
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat ante id elit cursus, a
+                            interdum nunc venenatis.
+                            Cras nulla nibh, euismod non turpis a, ullamcorper feugiat leo. Pellentesque elementum
+                            sapien tellus, quis blandit
+                            tortor malesuada eget. Integer vel neque in dolor euismod fermentum et a nulla. Pellentesque
+                            ultricies est
+                            dignissim malesuada aliquet. Nullam non ornare nibh. Orci varius natoque penatibus et magnis
+                            dis parturient montes,
                             nascetur ridiculus mus.
                         </Grid>
                     </Grid>
@@ -116,7 +224,7 @@ export default function LandingPage() {
                     >
                         Submit Application
                     </Button>
-                </MaterialUIForm>
+                </form>
             </div>
         </Container>
     );
